5878de9a232c0b712243a65cfa0262fd7a28798d
Angelika Maciazek <maciazekangelika@gmail.com>
2019-08-19 10:55:24 +0200

    simulation of fetching data - step1

--- src/products/containers/ProductsList.js
+++ src/products/containers/ProductsList.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react'
 
 import Product from '../components/Product'
 
+
+const products = [
+  {name:"One", description:"Super product 1", price:"123"},
+  {name:"Two", description:"Super product 2", price:"456"},
+  {name:"Three", description:"Super product 3", price:"789"}
+]
+
 class ProductsList extends Component {
 
   state = {
-    products: [
-      {name:"One", description:"Super product 1", price:"123"},
-      {name:"Two", description:"Super product 2", price:"456"},
-      {name:"Three", description:"Super product 3", price:"789"}
-    ]
+    products: []    
+  }
+
+  componentDidMount() {
+    this.setState({
+      products: products
+    })
   }
 
   render() {
