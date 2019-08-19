be35aef2ea9341505199e1f718b9464f4384c4d8
Angelika Maciazek <maciazekangelika@gmail.com>
2019-08-19 10:21:39 +0200

    initial components- refactoring

--- src/products/containers/ProductsList.js
+++ src/products/containers/ProductsList.js
@@ -7,9 +7,9 @@ class ProductsList extends Component {
   render() {
     return (
       <React.Fragment>
-        <Product name="One" decription="Super product 1" price="123" />
-        <Product name="Two" decription="Super product 2" price="456" />
-        <Product name="Three" decription="Super product 3" price="789" />
+        <Product data={{name:"One", decription:"Super product 1", price:"123"}} />
+        <Product data={{name:"Two", decription:"Super product 2", price:"456"}} />
+        <Product data={{name:"Three", decription:"Super product 3", price:"789"}} />
       </React.Fragment>
 
     )
