c679c5a24609c02b6ec496d76878750a462931d7
Angelika Maciazek <maciazekangelika@gmail.com>
2019-08-19 10:52:29 +0200

    move data to component state

--- src/products/containers/ProductsList.js
+++ src/products/containers/ProductsList.js
@@ -4,12 +4,22 @@ import Product from '../components/Product'
 
 class ProductsList extends Component {
 
+  state = {
+    products: [
+      {name:"One", description:"Super product 1", price:"123"},
+      {name:"Two", description:"Super product 2", price:"456"},
+      {name:"Three", description:"Super product 3", price:"789"}
+    ]
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Product data={{name:"One", description:"Super product 1", price:"123"}} />
-        <Product data={{name:"Two", description:"Super product 2", price:"456"}} />
-        <Product data={{name:"Three", description:"Super product 3", price:"789"}} />
+        {
+          this.state.products.map((product, index) => (
+            <Product key={`prod-${index}`} data={product}/>
+          ))
+        }
       </React.Fragment>
 
     )
