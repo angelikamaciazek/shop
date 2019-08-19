28b2990a1312189d16a98e40d241e6122616e15f
Angelika Maciazek <maciazekangelika@gmail.com>
2019-08-19 11:11:54 +0200

    before redux

---
+++ src/products/reducers.js
@@ -0,0 +1,23 @@
+const INITIAL_STATE = {
+  products: [],
+  product: {},
+  basket: {}
+}
+
+const products = [
+  { name: "One", description: "Super product 1", price: "123" },
+  { name: "Two", description: "Super product 2", price: "456" },
+  { name: "Three", description: "Super product 3", price: "789" }
+]
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'PRODUCTS/FETCH':
+      return {
+        ...state,
+        products: products,
+      }
+    default:
+      return state
+  }
+}
